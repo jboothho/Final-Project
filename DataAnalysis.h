4abfc0e31d0a16a2c28917d2b0100a6dcd669721
jboothho <julia.booth-howe@uvm.edu>
2024-04-23 17:18:38 -0400

    Updating header file for data analysis

--- DataAnalysis.h
+++ DataAnalysis.h
@@ -1,1 +1,29 @@
-//just creating this file 
+#import <iostream>
+
+/* DataAnalysis class that will take in a file, sort it, and perform three different
+ statistical analyses */
+
+class DataAnalysis {
+public:
+    // Constructors
+    DataAnalysis();
+    // Getters
+    
+    // Setters
+    
+    /* ReadDataFromFile function that will take in data from the .csv file */
+    
+    /* PerformAnalysis1 function that will perform the first analysis on the data*/
+    
+    /* PerformAnalysis2 function that will perform the second analysis on the data*/
+    
+    /* PerformAnalysis3 function that will perform the third analysis on the data*/
+    
+    /* SendData function that will send the data to Python to add to maps*/
+    
+    
+    
+private:
+    // vector holding dataset
+}
+

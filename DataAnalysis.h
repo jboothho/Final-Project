afed82f71d55d3dfad453575b7cbd7a39291f6d7
jboothho <julia.booth-howe@uvm.edu>
2024-04-24 09:52:58 -0400

    Started making function to load in data to vector, unsure if I'm gonna have to do different ones for each

--- DataAnalysis.h
+++ DataAnalysis.h
@@ -1,4 +1,8 @@
-#import <iostream>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+#include <string>
 
 /* DataAnalysis class that will take in a file, sort it, and perform three different
  statistical analyses */
@@ -12,12 +16,29 @@ public:
     // Setters
     
     /* ReadDataFromFile function that will take in data from the .csv file */
+    void ReadDataFromFile(vector<DataAnalysis> &data, string filepath){
+       
+        ifstream inFile;
+        inFile.open(filepath);
+        
+        string header;
+        
+        if (inFile){
+            inFile >> header;
+        }
+        //else-- file open error
+        
+    
+    }
     
     /* PerformAnalysis1 function that will perform the first analysis on the data*/
+        /* I'm thinking that one of them could perform a basic analysis and get the mean of each area*/
     
     /* PerformAnalysis2 function that will perform the second analysis on the data*/
+        /* This one could do a linear regression*/
     
     /* PerformAnalysis3 function that will perform the third analysis on the data*/
+        /* And this one could do a correlation assessment between two variables*/
     
     /* SendData function that will send the data to Python to add to maps*/
     

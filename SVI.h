a98981c30a330e2545edfe3fd2a0139bc2b241da
jboothho <julia.booth-howe@uvm.edu>
2024-05-01 18:51:23 -0400

    changed some documents i literally don't know

--- SVI.h
+++ SVI.h
@@ -1,1 +1,17 @@
-//made file
+#include "DataAnalysis.h"
+
+class SVI : public DataAnalysis {
+public:
+    /**
+     * Requires:
+     * Modifies:
+     * Effects:
+     */
+    SVI();
+    /**
+     * Requires:
+     * Modifies:
+     * Effects:
+     */
+    SVI(int ID, )
+}

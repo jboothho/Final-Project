aa7d89fd55e12a1ed57bb7a9b17d7e6f1d51e714
jboothho <julia.booth-howe@uvm.edu>
2024-05-01 16:15:44 -0400

    defined functions in UrbanHeat.cpp

--- UrbanHeat.cpp
+++ UrbanHeat.cpp
@@ -1,9 +1,9 @@
-#include "UrbanHe.h"
+#include "UrbanHeat.h"
 
 class UrbanHeat : public DataAnalysis {
 public:
     
-    Urban_Heat:: Urban_Heat(){
+    UrbanHeat:: UrbanHeat(){
         this -> ID = 0;
         this -> lst = 0;
         this -> can = 0;
@@ -11,7 +11,7 @@ public:
         this -> isa = 0;
     }
     
-    Urban_Heat::Urban_Heat(int ID, double lst, double can, double alb, double isa)
+    UrbanHeat::UrbanHeat(int ID, double lst, double can, double alb, double isa)
         : ID(ID), lst(lst), canopy(can), albedo(alb), isa(isa) {}
     
     // Getters
@@ -21,19 +21,56 @@ public:
         double getAlbedo() const { return albedo; }
         double getISA() const { return isa; }
 
-        // Setters
+    // Setters
         void setID(int newID) { ID = newID; }
         void setLST(double newLST) { lst = newLST; }
         void setCanopy(double newCanopy) { canopy = newCanopy; }
         void setAlbedo(double newAlbedo) { albedo = newAlbedo; }
         void setISA(double newISA) { isa = newISA; }
-
+    
+    void getDataFromFile(vector <UrbanHeat> &data){
+        ifstream inFile;
+        inFile.open("/Users/jboothhowe/Documents/CS2300/Final-Project/UrbanLCH.csv");
+        string header;
+        
+        if (inFile){
+            inFile >> header;
+        }
+        int ID = 0;
+        double lst = 0;
+        double canopy = 0;
+        double albedo = 0;
+        double isa = 0;
+        char comma = ',';
+        
+        while (inFile && inFile.peek() != EOF){
+            // get ID
+            inFile >> ID >> comma;
+            
+            // get Land Surface Temperature
+            inFile >> lst >> comma;
+            
+            // get Tree Canopy fraction
+            inFile >> canopy >> comma;
+            
+            // get Albedo
+            inFile >> albedo >> comma;
+            
+            // get Impervious surface fraction
+            inFile >> isa >> comma;
+            
+            // Putting data from this line of file into
+            // an UrbanHeat object and adding object to the vecotr
+            data.push_back(UrbanHeat(ID, canopy, albedo, isa));
+        }
+        inFile.close();
+    }
+    
     private:
         int ID;
         double lst;
         double canopy;
         double albedo;
         double isa;
-    
     
 }
